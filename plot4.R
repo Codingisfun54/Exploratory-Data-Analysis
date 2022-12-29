c5dd630ea60e03def7905d4f418942c13f6b40a9
BigBaller8 <55932897+BigBaller8@users.noreply.github.com>
2022-12-29 00:23:02 -0500

    r

--- plot4.R
+++ plot4.R
@@ -23,5 +23,5 @@ with(subsetdata, plot(NewDate, Sub_metering_1, type="l", xlab="", ylab="Energy s
 lines(subsetdata$NewDate, subsetdata$Sub_metering_2,type="l", col= "red")
 lines(subsetdata$NewDate, subsetdata$Sub_metering_3,type="l", col= "blue")
 legend(c("topright"), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty= 1, lwd=2, col = c("black", "red", "blue"))
-with(subsetdata, plot(FullTimeDate, Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power"))
+with(subsetdata, plot(NewDate, Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power"))
 dev.off()
